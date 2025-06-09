7768a621d80da3393ad11cb25560207b536dfff5
Arthur Santiago Mayolino <arthursmpc@protonmail.com>
2025-06-09 19:22:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// function newQuestion(difficulty) {
+//     if difficulty === 1
+// }
+const textoPergunta = document.getElementById("question");
+
+textoPergunta.textContent = "Teste de pergunta 1";
